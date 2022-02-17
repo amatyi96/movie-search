d1a03eee5fc858e90d6267f4369be174188b8391
Albirt Mátyás <albirt.matyas@convkit.com>
2022-02-17 18:50:20 +0100

    Remove console log

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,10 @@
-import {Component, OnDestroy} from '@angular/core';
-import {MovieService} from "./services/movie.service";
-import {Subscription} from "rxjs";
+import {Component} from '@angular/core';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnDestroy {
-
-    private genreListSubscription: Subscription;
-
-    constructor(private $movieService: MovieService) {
-        this.genreListSubscription = this.$movieService.getGenreList().subscribe({
-            next(response) {console.log(response);},
-            error(error) {console.log(error);}
-        });
-    }
-
-    public ngOnDestroy(): void {
-        this.genreListSubscription.unsubscribe();
-    }
+export class AppComponent {
+    constructor() {}
 }
